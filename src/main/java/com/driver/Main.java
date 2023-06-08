247fc0ff39562cea48e43082dbc698343ce00bda
Abhay Kumar <kabhay166@gmail.com>
2023-06-08 18:16:55 +0530

    "Polymorphism Assignment"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    static class Product{
+        public int product(int x, int y) {
+            return x *y;
+        }
+        public int product(int x, int y, int z) {
+            int ans = product(x,y);
+            return  ans * z;
+        }
+        public double product(double x, double y) {
+            return x *y;
+        }
+
+    }
+    public static void main(String[] args) {
+
+        // Creating object of product class
+        Product p = new Product();
+
+        // calling the methods of product class
+        System.out.println("Product of 2 and 3 : " + p.product(2,3));
+        System.out.println("Product of 2 , 3 and 4 : " + p.product(2,3,4));
+        System.out.println("Product of 5.5  and  3.0 : " +p.product(5.5,3.0));
+    }
 
 }
